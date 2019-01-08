70bda3e1a69aaf37da43265bb6a5714986d08f59
insidemirage <vladislavlightinthedarkness@gmail.com>
2019-01-09 02:36:47 +0300

    c logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var counter = 0;
 var before = 0;
+var clickcounter = 0; // Число сделанных кликов
+var lastcard; // Индекс предыдущей карточки
 function Random(mass){
     let r = Math.round(Math.random() * (11 - 0) + 0); // Выбираем рандомный элемент массива от 11 до 0
     if(mass[r]==undefined){
@@ -8,6 +10,10 @@ function Random(mass){
     else{
         return Random(mass); // Если задан, возвращаемся в начало функции
     }
+}
+function DeleteCard(card, cardt){
+    $('.card').eq(card).css('opacity','0'); //Функция просто скрывает элемент(это позволяет избежать сдвигов)
+    $('.card').eq(cardt).css('opacity','0');
 }
 $(document).ready(function(){
     
@@ -19,4 +25,15 @@ $(document).ready(function(){
        }
     });
     console.log(answers);
+    $('.card').click(function(){
+        clickcounter++;
+        if(clickcounter==2){ // Если произошел клик на второй карте
+            clickcounter = 0;
+            if(answers[lastcard]==answers[$(this).index()]) DeleteCard(lastcard, $(this).index()); //Если совпали , то удаляем карточки 
+        }
+        else{
+            lastcard = $(this).index(); // Сброс предыдущей карты
+
+        }
+    });
 });
