9b807cf835ee1630ef623b38046b8ba4f7ca2d50
insidemirage <vladislavlightinthedarkness@gmail.com>
2019-01-09 02:21:24 +0300

    2 step : logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+var counter = 0;
+var before = 0;
+function Random(mass){
+    let r = Math.round(Math.random() * (11 - 0) + 0); // Выбираем рандомный элемент массива от 11 до 0
+    if(mass[r]==undefined){
+        return r; // Если элемент не задан(undefined) то возвращаем выбраный
+    }
+    else{
+        return Random(mass); // Если задан, возвращаемся в начало функции
+    }
+}
 $(document).ready(function(){
-    // $('.card').click(function(){
-    //     $(this).css('opacity','0');
-    // });
+    
+    var answers = new Array(12); // Массив для ответов
+    $.each(answers, function(index,elem){
+        answers[Random(answers)] = counter; //К выбранному с помощью функции элементу присваиваем ответ
+        if(index != 0 && index % 2 != 0 ){ // Если номер элемента не 0 и является нечетным(каждый второй элемент), то увеличиваем счетчик
+            counter++;
+       }
+    });
+    console.log(answers);
 });
